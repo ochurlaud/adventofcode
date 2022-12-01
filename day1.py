a2e42bd2f8f47b3a688892def77f6e6f34456710
Olivier CHURLAUD <olivier@churlaud.com>
2022-12-02 00:03:16 +0100

    Day 1: Short version

--- day1.py
+++ day1.py
@@ -3,17 +3,8 @@
 INPUT_FILE = "inputs/day1.txt"
 
 if __name__ == "__main__":
-    elf_calory_list = []
     with open(INPUT_FILE) as f:
-        calory_list = []
-        for line in f:
-            if line == "\n":
-                elf_calory_list.append(calory_list)
-                calory_list = []
-            else:
-                calory_list.append(int(line[:-1]))
-        if calory_list:
-            elf_calory_list.append(calory_list)
+        elf_calory_list = [ [int(i) for i in s.split('\n')] for s in f.read()[:-1].split('\n\n') ]  # [:-1] to remove the last '\n'
         
     print(max([sum(l) for l in elf_calory_list]))
     print(sum(sorted([sum(l) for l in elf_calory_list])[-3:]))
