c7555900821eb315978dcb1af3c127dc32fc6a44
Olivier CHURLAUD <olivier@churlaud.com>
2022-12-04 00:35:24 +0100

    Day3: victory !

---
+++ day3.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+INPUT_FILE = 'inputs/day3.txt'
+
+def priority(c):
+    if c.isupper():
+        return ord(c) - ord('A') + 27  # A = 27, B = 28,...
+    else:
+        return ord(c) - ord('a') + 1  # a = 1, b = 2,...
+
+with open(INPUT_FILE) as f:
+    backpacks = f.read().splitlines()
+
+duplicate_items = []
+for b in backpacks:
+    bsize = len(b)
+    compartiment1 = b[:bsize//2]
+    compartiment2 = b[bsize//2:]
+    current_duplicate_items = []
+    for item in compartiment1:
+        if item in compartiment2 and item not in current_duplicate_items:
+            current_duplicate_items.append(item)
+    duplicate_items += current_duplicate_items
+ps = [priority(x) for x in duplicate_items]
+print("part 1:", sum(ps))
+
+duplicate_items = []
+for k in range(len(backpacks)//3):
+    current_duplicate_items = []
+    for item in backpacks[3*k]:
+        if item in backpacks[3*k+1] and item in backpacks[3*k+2] and item not in current_duplicate_items:
+            current_duplicate_items.append(item)
+    duplicate_items += current_duplicate_items
+
+ps = [priority(x) for x in duplicate_items]
+print("part2:", sum(ps))
