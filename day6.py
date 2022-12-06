6868eaa5f109aefc0ffd9822f6579513148f8ec4
Olivier Churlaud <olivier.churlaud@cnes.fr>
2022-12-06 14:14:56 +0100

    day6: victooory

---
+++ day6.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+print("## AdventOfCode 2022: day6")
+
+INPUT_FILE = "inputs/day6.txt"
+
+EXAMPLE = """mjqjpqmgbljsphdztnvjfqwrcgsmlb"""
+EXAMPLE = """nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"""
+with open(INPUT_FILE) as f:
+    rawdata = f.read()
+
+#rawdata = EXAMPLE
+
+
+def start_marker_position(data, size):
+    start_block = []
+    for i, char in enumerate(data):
+        if char in start_block: 
+            index = start_block.index(char)
+            start_block = start_block[index+1:] + [char]
+        else:
+            start_block.append(char)
+            if len(start_block) == size:
+                return i + 1
+
+START_BLOCK_SIZE = 4
+print("Part 1:", start_marker_position(rawdata, START_BLOCK_SIZE))
+START_BLOCK_SIZE = 14
+print("Part 2:", start_marker_position(rawdata, START_BLOCK_SIZE))
+
