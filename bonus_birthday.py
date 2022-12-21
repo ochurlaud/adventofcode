2b0a336acb196ca7a469ba571ca7108b4877794c
clemshubs <clement.hubin@gmail.com>
2022-12-21 08:47:59 +0100

    Merci <3 (en plus propre)

--- bonus_birthday.py
+++ bonus_birthday.py
@@ -1,9 +1,9 @@
 # Elves encrypting their message
-RDM_STR="ORHATPOPKATYSBARNITRESTTIERDAHEDTKCSAAZYSY"
+RDM_STR = "ORHATPOPKATYSBARNITRESTTIERDAHEDTKCSAAZYSY"
 
 # Advent of code vibe
 def prime(x):
-    if x in [2, 3, 5, 7, 11, 13, 17, 19, 23 , 29, 31,37,41,43]:
+    if x in [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43]:
         return True
     else:
         return False
